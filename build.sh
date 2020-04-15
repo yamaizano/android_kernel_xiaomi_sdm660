d4a98b8209aa7a741f5b686a6e0f9666a1d41522
Pzqqt <821026875@qq.com>
2020-04-15 09:25:07 +0800

    build.sh: built with LiuNian Clang 20200415

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ rm -f $ZIMG
 export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
-export CLANG_PATH=/home/pzqqt/build_toolchain/LiuNian_Clang_20200402
+export CLANG_PATH=/home/pzqqt/build_toolchain/LiuNian_Clang_20200415
 export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 export KBUILD_BUILD_HOST="lenovo"
