b0d47452ece9b9c941ffc374c99de6b50f18ebb0
Yaroslav Furman <yaro330@gmail.com>
2021-03-07 21:03:44 +0300

    fuse: dev: Don't wait forever in request_wait_answer
    Device: sweet
    Waiting for too long causes errors like this:
    
    [ 2602.785091] Freezing of tasks failed after 10.015 seconds (1 tasks refusing to freeze, wq_busy=0):
    [ 2602.787743] usage data...] D    0 11550    735 0x00400809
    [ 2602.787763] Call trace:
    [ 2602.787803] __switch_to+0x128/0x144
    [ 2602.787827] __schedule+0x744/0x91c
    [ 2602.787839] schedule+0x70/0x90
    [ 2602.787860] __fuse_request_send+0x26c/0x3a4
    [ 2602.787870] fuse_simple_request+0x170/0x1c4
    [ 2602.787885] fuse_lookup_name+0xe4/0x234
    [ 2602.787895] fuse_lookup+0x50/0x17c
    [ 2602.787911] lookup_slow+0xd8/0x170
    [ 2602.787923] walk_component+0x19c/0x4dc
    [ 2602.787932] path_lookupat+0x7c/0x130
    [ 2602.787940] filename_lookup+0x8c/0x194
    [ 2602.787948] user_path_at_empty+0x44/0x54
    [ 2602.787963] SyS_faccessat+0xa8/0x280
    [ 2602.787973] __sys_trace_return+0x0/0x4
    
    Signed-off-by: mARk <r3066.funtab@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -487,7 +487,8 @@ static void request_wait_answer(struct fuse_conn *fc, struct fuse_req *req)
 	 * Either request is already in userspace, or it was forced.
 	 * Wait it out.
 	 */
-	wait_event(req->waitq, test_bit(FR_FINISHED, &req->flags));
+	wait_event_freezable_timeout(req->waitq,
+			test_bit(FR_FINISHED, &req->flags), freeze_timeout_msecs / 2);
 }
 
 static void __fuse_request_send(struct fuse_conn *fc, struct fuse_req *req)
