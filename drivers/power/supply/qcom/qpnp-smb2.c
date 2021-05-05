664963910a0b9af56a4e4653c705810019434c07
Manish4586 <manish.n.manish45@gmail.com>
2021-05-05 09:48:45 +0530

    power: smb: unlock qc3.0 for global boards
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: yamaizano <yukizakimiyamura@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -317,7 +317,7 @@ static int smb2_parse_dt(struct smb2 *chip)
 						"qcom,pd-not-supported");
 
 	if (hwc_check_global || hwc_check_cn) {
-		chg->batt_profile_fcc_ua = 2300000;
+		chg->batt_profile_fcc_ua = 2900000;
 #ifdef CONFIG_XIAOMI_TULIP
 		if (is_poweroff_charge) {
 			if (hwc_check_india)
--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -4270,7 +4270,10 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		break;
 	case POWER_SUPPLY_TYPE_USB_HVDCP_3:
 #if defined (CONFIG_XIAOMI_WHYRED) || defined(CONFIG_XIAOMI_TULIP)
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 2000000);
+		if (hwc_check_global || hwc_check_cn) {
+			vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 2900000);
+		} else
+			vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 2000000);
 #endif
 		break;
 	default:
