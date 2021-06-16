bb80db60104d010393cb14629d51aea49a6f0f3a
Pzqqt <821026875@qq.com>
2021-06-16 08:48:03 +0800

    build.sh: Compile with Google clang 12.0.7

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ rm -f $ZIMG
 export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
-export CLANG_PATH=/home/pzqqt/build_toolchain/clang-r416183c-12.0.6
+export CLANG_PATH=/home/pzqqt/build_toolchain/clang-r416183d-12.0.7
 export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 export KBUILD_BUILD_HOST="wsl2"
