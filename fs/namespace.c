63567cdff06e22d09206ce8d0c041b48ae12f37a
yamaizano <muhammad.ikhsan.tkom421@polban.ac.id>
2025-01-15 16:15:22 +0700

    fs: backport path_umount from linuv v5.9.16
    references: https://elixir.bootlin.com/linux/v5.9.16/A/ident/path_umount

--- fs/namespace.c
+++ fs/namespace.c
@@ -1708,6 +1708,21 @@ static inline bool may_mandlock(void)
 }
 #endif
 
+int path_umount(struct path *path, int flags)
+{
+	struct mount *mnt = real_mount(path->mnt);
+	int ret;
+
+	ret = can_umount(path, flags);
+	if (!ret)
+		ret = do_umount(mnt, flags);
+
+	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
+	dput(path->dentry);
+	mntput_no_expire(mnt);
+	return ret;
+}
+
 /*
  * Now umount can handle mount points as well as block devices.
  * This is important for filesystems which use unnamed block devices.
