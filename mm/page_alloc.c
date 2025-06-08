a1028fbef407c4c0231fe26982be46294cbecf33
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-06-08 07:40:34 +0700

    Revert simple_lmk
    5746fe47d8a7 defconfig: adjust slmk minfree and timeout
    088175258132 defconfig: enable CONFIG_ANDROID_SIMPLE_LMK
    e3d6d01b7efe simple_lmk: Reap anonymous memory from victims
    b2c996c659a0 simple_lmk: Reduce unnecessary wake ups
    146c83bcd024 simple_lmk: Ratelimit the 'no processes available to kill' message
    1c098fc6f0bd simple_lmk: Fix victim scheduling priority elevation
    2cba01f99af1 simple_lmk: Thaw victims upon killing them
    c7387d389f49 simple_lmk: Make the reclaim thread freezable
    1aed53270805 simple_lmk: Be extra paranoid if tasks can have no pages
    41cef4df791c mm: vmpressure: Fix rampant inaccuracies caused by stale data usage
    b710419f330d mm: vmpressure: Fix a race that would erroneously clear accumulated data
    deeb2f8a307c mm: vmpressure: Ignore costly-order allocations for direct reclaim too
    12a405516b44 simple_lmk: Update do_send_sig_info() group argument for 4.18+
    8f0e69c9698d simple_lmk: Optimize victim finder to eliminate hard-coded adj ranges
    c4ac26165af5 simple_lmk: Cacheline-align the victims array and mm_free_lock on SMP
    2bec3d826562 simple_lmk: Pass a custom swap function to sort()
    0e6000a931e5 simple_lmk: Skip victim reduction when all victims need to be killed
    5526573da519 simple_lmk: Use MIN_FREE_PAGES wherever pages_needed is used
    cd65e9f6c273 simple_lmk: Don't block in simple_lmk_mm_freed() on mm_free_lock
    5069ef62d6c5 mm: vmpressure: Don't export tunables to userspace
    c647c506cec8 simple_lmk: Update Kconfig description for VM pressure change
    c8d785547fc9 simple_lmk: Add !PSI dependency
    0ca79eb36632 simple_lmk: Print a message when the timeout is reached
    17c6e25899a4 simple_lmk: Remove unnecessary clean-up when timeout is reached
    007c4aa4fa65 simple_lmk: Hold an RCU read lock instead of the tasklist read lock
    2cfc68e2b6e9 simple_lmk: Consider all positive adjs when finding victims
    45980f2cfa04 mm: vmpressure: Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER
    b1704638170c mm: Adjust tsk_is_oom_victim() for Simple LMK
    37293e474179 mm: vmpressure: Don't cache the window size
    67b8c82f5c1c mm: vmpressure: Interpret zero scanned pages as 100% pressure
    1e7ca618c9ee mm: vmpressure: Don't exclude any allocation types
    4e72439a9425 simple_lmk: Update adj targeting for Android 10
    7f40674a2f7c simple_lmk: Use vmpressure notifier to trigger kills
    3b52f4b09dc6 mm: vmpressure: account allocstalls only on higher pressures
    196bc0a9281c mm: vmpressure: scale pressure based on reclaim context
    bf9eb3d7ebf9 simple_lmk: Include swap memory usage in the size of victims
    68b6c06eba8f simple_lmk: Relax memory barriers and clean up some styling
    b6b32ce625ef simple_lmk: Place victims onto SCHED_RR
    7312cf35e86d simple_lmk: Add a timeout to stop waiting for victims to die
    fb53814e3c45 simple_lmk: Ignore tasks that won't free memory
    7e2f1cb18f3b simple_lmk: Simplify tricks used to speed up the death process
    a24f1324ec4b simple_lmk: Report mm as freed as soon as exit_mmap() finishes
    af21692769bd simple_lmk: Mark victim thread group with TIF_MEMDIE
    17245ea20831 simple_lmk: Disable OOM killer when Simple LMK is enabled
    a0d42cc92f27 simple_lmk: Print a message when there are no processes to kill
    171b2b0872bf simple_lmk: Remove compat cruft not specific to 4.14
    d6a7bfd40fb2 simple_lmk: Update copyright to 2020
    883c9f958f32 simple_lmk: Don't queue up new reclaim requests during reclaim
    6a81f4c4d84f simple_lmk: Increase default minfree value
    84df0f713504 simple_lmk: Clean up some code style nitpicks
    bf100c703fac simple_lmk: Make reclaim deterministic
    e86ad6d54c4d simple_lmk: Fix broken multicopy atomicity for victims_to_kill
    ef28e55c4284 simple_lmk: Use proper atomic_* operations where needed
    d4aa911d8961 simple_lmk: Remove kthread_should_stop() exit condition
    4f4572b84de1 simple_lmk: Fix pages_found calculation
    0d35a06f1dd3 simple_lmk: Introduce Simple Low Memory Killer for Android

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4340,7 +4340,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned int cpuset_mems_cookie;
 	unsigned int zonelist_iter_cookie;
 	int reserve_flags;
-	bool used_vmpressure = false;
 
 	/*
 	 * We also sanity check to catch abuse of atomic reserves being used by
@@ -4375,11 +4374,8 @@ restart:
 	if (!ac->preferred_zoneref->zone)
 		goto nopage;
 
-	if (gfp_mask & __GFP_KSWAPD_RECLAIM) {
-		if (!used_vmpressure)
-			used_vmpressure = vmpressure_inc_users(order);
+	if (gfp_mask & __GFP_KSWAPD_RECLAIM)
 		wake_all_kswapds(order, gfp_mask, ac);
-	}
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
@@ -4468,8 +4464,6 @@ retry:
 		goto nopage;
 
 	/* Try direct reclaim and then allocating */
-	if (!used_vmpressure)
-		used_vmpressure = vmpressure_inc_users(order);
 	page = __alloc_pages_direct_reclaim(gfp_mask, order, alloc_flags, ac,
 							&did_some_progress);
 	if (page)
@@ -4590,11 +4584,6 @@ fail:
 	warn_alloc(gfp_mask, ac->nodemask,
 			"page allocation failure: order:%u", order);
 got_pg:
-	if (used_vmpressure)
-		vmpressure_dec_users();
-	if (!page)
-		warn_alloc(gfp_mask, ac->nodemask,
-				"page allocation failure: order:%u", order);
 	return page;
 }
 
